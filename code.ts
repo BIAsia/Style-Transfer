1301c79ac297da7f70f26edbc61bda9db66eea95
biasia <980492289@qq.com>
2023-01-20 16:52:11 +0800

    import text styles

--- code.ts
+++ code.ts
@@ -7,7 +7,10 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+figma.ui.resize(300, 500)
 
+let allPaintStyles: PaintStyle[], allTextStyles: TextStyle[], allGridStyles: GridStyle[], allEffectStyles: EffectStyle[];
+const nodes: SceneNode[] = [];
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
@@ -15,19 +18,86 @@ figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
   if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    GetAllStyles();
+    CreateTransfer();
   }
+
+  if (msg.type === 'import-styles') {
+    ImportStyles();
+  }
+
+  if (msg.quit) figma.closePlugin();
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
 };
+
+
+function GetAllStyles(){
+  allPaintStyles = figma.getLocalPaintStyles();
+  allTextStyles = figma.getLocalTextStyles();
+  allGridStyles = figma.getLocalGridStyles();
+  allEffectStyles = figma.getLocalEffectStyles();
+
+  console.log(allPaintStyles, allTextStyles, allGridStyles, allEffectStyles)
+
+}
+
+function CreateTransfer(){
+
+  allTextStyles.forEach((element, i) => {
+    createTextStyleLayer(element, 50, i*50)
+  });
+
+  // allPaintStyles.forEach(element => {
+  //   paintInfo.characters += JSON.stringify(element)
+  // });
+}
+
+function createTextStyleLayer(element:TextStyle, x:number, y:number){
+  (async () => {
+    const text = figma.createText()
+  
+    text.x = x
+    text.y = y
+  
+    // Load the font in the text node before setting the characters
+    await figma.loadFontAsync(element.fontName)
+    text.characters = element.name;
+    text.fontSize = element.fontSize;
+    text.textDecoration = element.textDecoration;
+    text.fontName = element.fontName;
+    text.letterSpacing = element.letterSpacing;
+    text.lineHeight = element.lineHeight;
+    text.paragraphIndent = element.paragraphIndent;
+    text.paragraphSpacing = element.paragraphSpacing;
+    text.textCase = element.textCase;
+
+
+    figma.currentPage.appendChild(text);
+    nodes.push(text);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  })()
+}
+
+function ImportStyles(){
+  let nodes = figma.currentPage.selection;
+  nodes.forEach(element => {
+    if (element.type == 'TEXT'){
+      (async () => {
+        await figma.loadFontAsync(element.fontName as FontName)
+        const text = figma.createTextStyle();
+        text.name = element.characters;
+        text.fontSize = element.fontSize as number;
+        text.textDecoration = element.textDecoration as TextDecoration;
+        text.fontName = element.fontName as FontName;
+        text.letterSpacing = element.letterSpacing as LetterSpacing;
+        text.lineHeight = element.lineHeight as LineHeight;
+        text.paragraphIndent = element.paragraphIndent as number;
+        text.paragraphSpacing = element.paragraphSpacing as number;
+        text.textCase = element.textCase as TextCase;
+      })()
+    }
+  });
+}
